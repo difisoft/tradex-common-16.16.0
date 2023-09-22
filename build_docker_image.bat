0c4dd56f69b1de854445f73008fb31c01aac7d80
Nguyễn Trí Tuấn Hiệp <hiepnguyen@difisoft.com>
2023-09-22 17:57:03 +0700

    Init

--- build_docker_image.bat
+++ build_docker_image.bat
@@ -1,1 +1,1 @@
-docker build -f dockerfile-local.txt --tag aaa .
+docker build -f dockerfile-local.txt --tag node-kafka:16.16.0 .
--- build_with_docker.bat
+++ build_with_docker.bat
@@ -1,1 +1,1 @@
-docker run -v "%CD%:/app/" --network=host -ti aaa /bin/bash
+docker run -v "%CD%:/app/" --network=host -ti node-kafka:16.16.0 --name tradex-common:16.16.0 /bin/bash
